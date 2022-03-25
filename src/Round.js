99aeb99c4c2f42fabd36a4b58504c1f03a81f7f4
Oakley Windiate <oakley.windiate@gmail.com>
2022-03-24 21:06:49 -0600

    Create Round class

---
+++ src/Round.js
@@ -0,0 +1,11 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
+    this.index = deck.deck
+  }
+  
+module.exports = Round;
